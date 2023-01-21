da9b77069f3560045d8e9c8e62273379c19b8040
volesen <vincolesen@gmail.com>
2023-01-21 19:23:34 +0100

    Implement state (variables, blocks, scoping)

--- src/com/volesen/lox/Lox.java
+++ src/com/volesen/lox/Lox.java
@@ -45,17 +45,16 @@ public class Lox {
             hadError = false;
         }
     }
-
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
         Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
+        List<Stmt> statements = parser.parse();
 
         // Stop if there was a syntax error.
         if (hadError) return;
 
-        interpreter.interpret(expression);
+        interpreter.interpret(statements);
     }
 
     static void error(int line, String message) {
