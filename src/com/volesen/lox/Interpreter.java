962dcf3bb6d3abda42829013da1da0f91e4cb873
volesen <vincolesen@gmail.com>
2023-06-23 14:14:47 +0200

    Implemented function definition semantics

--- src/com/volesen/lox/Interpreter.java
+++ src/com/volesen/lox/Interpreter.java
@@ -44,7 +44,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         stmt.accept(this);
     }
 
-    private void executeBlock(List<Stmt> statements, Environment environment) {
+    void executeBlock(List<Stmt> statements, Environment environment) {
         Environment previous = environment.enclosing;
         try {
             this.environment = environment;
@@ -66,6 +66,13 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
         evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitFunctionStmt(Stmt.Function stmt) {
+        LoxFunction function = new LoxFunction(stmt);
+        environment.define(stmt.name.lexeme, function);
         return null;
     }
 
