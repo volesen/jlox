f72065998f76f154a4a96e771780d25017d6af3d
volesen <vincolesen@gmail.com>
2023-06-23 14:21:11 +0200

    Added return statement parsing

--- src/com/volesen/tool/GenerateAst.java
+++ src/com/volesen/tool/GenerateAst.java
@@ -29,9 +29,9 @@ public class GenerateAst {
                 "Block      : List<Stmt> statements",
                 "Expression : Expr expression",
                 "Function   : Token name, List<Token> params, List<Stmt> body",
-                "If         : Expr condition, Stmt thenBranch," +
-                        " Stmt elseBranch",
+                "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
                 "Print      : Expr expression",
+                "Return     : Token keyword, Expr value",
                 "Var        : Token name, Expr initializer",
                 "While      : Expr condition, Stmt body"
         ));
