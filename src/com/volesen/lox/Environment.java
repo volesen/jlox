6a23b59e51044fdbe9e42c765c87c50f9965d3a2
volesen <vincolesen@gmail.com>
2023-06-25 19:28:30 +0200

    Added a resolution and binding pass

--- src/com/volesen/lox/Environment.java
+++ src/com/volesen/lox/Environment.java
@@ -22,8 +22,7 @@ public class Environment {
 
         if (enclosing != null) return enclosing.get(name);
 
-        throw new RuntimeError(name,
-                "Undefined variable '" + name.lexeme + "'.");
+        throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
     void assign(Token name, Object value) {
@@ -37,12 +36,28 @@ public class Environment {
             return;
         }
 
-        throw new RuntimeError(name,
-                "Undefined variable '" + name.lexeme + "'.");
+        throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
+
     void define(String name, Object value) {
         values.put(name, value);
     }
 
+    Environment ancestor(Integer distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            assert environment != null;
+            environment = environment.enclosing;
+        }
+        return environment;
+    }
+
+    Object getAt(Integer distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
 
 }
--- src/com/volesen/lox/Lox.java
+++ src/com/volesen/lox/Lox.java
@@ -54,6 +54,12 @@ public class Lox {
         // Stop if there was a syntax error.
         if (hadError) return;
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
+        // Stop if there was a semantic error.
+        if (hadError) return;
+
         interpreter.interpret(statements);
     }
 
